218546656f3c522f7ba4e00ec9a8b29f7f1310ef
yjw9397 <ykm9397@naver.com>
2022-10-28 16:08:11 +0900

    push

--- 01_skeleton/accounts/views.py
+++ 01_skeleton/accounts/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.shortcuts import redirect, render
 from django.views.decorators.http import require_http_methods, require_POST
 from django.contrib.auth import get_user_model
+from django.http import JsonResponse
+
 
 from .forms import CustomUserChangeForm, CustomUserCreationForm
 
@@ -116,11 +118,18 @@ def follow(request, user_pk):
         User = get_user_model()
         me = request.user
         you = User.objects.get(pk=user_pk)
+
         if me != you:
             if you.followers.filter(pk=me.pk).exists():
                 you.followers.remove(me)
-            else:
+                is_followed = False
+            else:  
                 you.followers.add(me)
-        return redirect('accounts:profile', you.username)
+                is_followed = True
+        context = {
+            'is_followed': is_followed,
+            'followers_count': you.followers.count(),
+            'followings_count': you.followings.count(),              
+        }
+        return JsonResponse(context)
     return redirect('accounts:login')
-
